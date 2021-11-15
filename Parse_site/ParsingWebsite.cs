809ba44ee63efbec6abe5eb7eef0f1ee5002ad2e
Дмитро Мінаєв <Dmytro.Minaiev@cs.khpi.edu.ua>
2021-11-15 16:53:37 +0200

    add new list

--- Parse_site/ParsingWebsite.cs
+++ Parse_site/ParsingWebsite.cs
@@ -7,8 +7,9 @@ namespace Parse_site
 {
     class ParsingWebsite
     {
-        List<(string url, double response)> listUrlsSitemap = null;
-        List<(string url, double response)> listUrlsHtml = null;
+        List<string> listUrlsSitemap = null;
+        List<string> listUrlsHtml = null;
+        List<(string url, double response)> listUrls = null;
 
         ParseHtmlDocument parserHtml;
         ParseSitemapXml parserSitemap;
@@ -33,61 +34,119 @@ namespace Parse_site
 
             listUrlsSitemap = await parserSitemap.ParseAsync();
 
+            listUrls = new List<(string url, double response)>(listUrlsSitemap?.Count ?? 0 + listUrlsHtml?.Count ?? 0);
+
             ResultsProcessing();
+            G();
             ShowResults();
         }
 
         private void ResultsProcessing()
         {
-            ComparerListUrls comparerList = new ComparerListUrls();
-
-            if (listUrlsSitemap != null && listUrlsSitemap.Count != 0)
+            if (listUrlsSitemap != null)
             {
-                List<(string url, double response)> list_html = new List<(string url, double response)>(listUrlsHtml);
+                List<string> list_html = new List<string>(listUrlsHtml);
 
-                foreach (var tuplesHtml in list_html)
+                foreach (var urlHtml in list_html)
                 {
-                    foreach (var tuplesXml in listUrlsSitemap)
+                    foreach (var urlXml in listUrlsSitemap)
                     {
-                        if (tuplesHtml.url == tuplesXml.url)
+                        if (urlHtml == urlXml)
                         {
-                            listUrlsHtml.Remove(tuplesHtml);
-                            listUrlsSitemap.Remove(tuplesXml);
+                            listUrlsHtml.Remove(urlHtml);
+                            listUrlsSitemap.Remove(urlXml);
                             break;
                         }
                     }
                 }
+            }
+        }
 
-                listUrlsSitemap.Sort(comparerList);
+        private void G()
+        {
+            System.Diagnostics.Stopwatch time = new System.Diagnostics.Stopwatch();
+
+            if (listUrlsSitemap != null)
+            {
+                foreach (var urlSitemap in listUrlsSitemap)
+                {
+                    try
+                    {
+                        time.Start();
+                        ((HttpWebRequest)WebRequest.Create(urlSitemap)).GetResponse().Close();
+                        time.Stop();
+
+                        listUrls.Add((urlSitemap, time.Elapsed.TotalMilliseconds));
+                    }
+                    catch (WebException)
+                    {
+                        listUrls.Add((urlSitemap + " --- not work", -1));
+                    }
+                    finally
+                    {
+                        time.Reset();
+                    }
+                }
             }
 
-            if (listUrlsHtml.Count != 0)
-                listUrlsHtml.Sort(comparerList);
+            if (listUrlsHtml != null)
+            {
+                foreach (var urlSitemap in listUrlsHtml)
+                {
+                    try
+                    {
+                        time.Start();
+                        ((HttpWebRequest)WebRequest.Create(urlSitemap)).GetResponse().Close();
+                        time.Stop();
+
+                        listUrls.Add((urlSitemap, time.Elapsed.TotalMilliseconds));
+                    }
+                    catch (WebException)
+                    {
+                        listUrls.Add((urlSitemap + " --- not work", -1));
+                    }
+                    finally
+                    {
+                        time.Reset();
+                    }
+                }
+            }
+
+            listUrls.Sort(new ComparerListUrls());
         }
 
         private void ShowResults()
         {
             int i = 1;
-            if (listUrlsSitemap != null && listUrlsSitemap.Count != 0)
+            if (listUrlsSitemap != null)
             {
                 Console.WriteLine("Sitemap xml:");
-                foreach (var tuple in listUrlsSitemap)
+                foreach (var url in listUrlsSitemap)
                 {
-                    Console.WriteLine("{0})Url: {1}\n Time response: {2}ms\n", i++, tuple.url, tuple.response);
+                    Console.WriteLine("{0})Url: {1}\n", i++, url);
                 }
 
                 i = 1;
                 Console.WriteLine("\n\n\n");
             }
 
-            if (listUrlsHtml != null && listUrlsHtml.Count != 0)
+            if (listUrlsHtml != null)
             {
                 Console.WriteLine("Html document:");
 
-                foreach (var tuple in listUrlsHtml)
+                foreach (var url in listUrlsHtml)
                 {
-                    Console.WriteLine("{0})Url: {1}\n Time response: {2}ms\n", i++, tuple.url, tuple.response);
+                    Console.WriteLine("{0})Url: {1}\n", i++, url);
                 }
+
+                i = 1;
+                Console.WriteLine("\n\n\n");
+            }
+
+            Console.WriteLine("Timing website(ms):");
+            foreach (var tuple in listUrls)
+            {
+                Console.WriteLine("{0})Url: {1}\n Time response: {2}ms\n", i++, tuple.url, tuple.response);
             }
 
             Console.WriteLine("\n\nUrls(html documents) found after crawling a website: {0}", listUrlsHtml?.Count ?? 0);
