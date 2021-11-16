5f2d24faba66a340d091a7f655c6bad980348654
Дмитро Мінаєв <Dmytro.Minaiev@cs.khpi.edu.ua>
2021-11-16 21:34:40 +0200

    .

--- Parse_site/ParseHtmlDocument.cs
+++ Parse_site/ParseHtmlDocument.cs
@@ -31,53 +31,181 @@ namespace Parse_site
             }
 
             Regex reg = new Regex("<a href=\"(.*?)\"", RegexOptions.Singleline);
+            Regex reg1 = new Regex("<a class=\".*?\" href=\"(.*?)\"", RegexOptions.Singleline);
             MatchCollection matchCollectionUrls = reg.Matches(htmlDocument);
 
             if (matchCollectionUrls.Count == 0)
                 return null;
 
             var listUrlsHtml = new List<string>(matchCollectionUrls.Count);
+            bool inAddlist = false;
 
-            foreach (Match matchUrl in matchCollectionUrls)
+            for (int i = 0; i < 2; i++)
             {
-                string url;
-
-                if (!matchUrl.Groups[1].Value.Contains("mailto:") && !matchUrl.Groups[1].Value.Contains("skype:"))
+                foreach (Match matchUrl in matchCollectionUrls)
                 {
-                    if (matchUrl.Groups[1].Value == "/" || matchUrl.Groups[1].Value == "#")
+                    string url = matchUrl.Groups[1].Value;
+
+                    if (url.Contains("mailto:") || url.Contains("skype:"))
                     {
-                        url = Uri.UriSchemeHttps + "://" + _uri.Host + '/';
+                        continue;
                     }
-                    else if (!matchUrl.Groups[1].Value.Contains("https://") && !matchUrl.Groups[1].Value.Contains("http://"))
+                    else if (url.Contains("http://") || url.Contains("https://"))
                     {
-                        try
+                        if (!(_uri.Host == new Uri(url).Host))
                         {
-                            ((HttpWebRequest)WebRequest.Create(Uri.UriSchemeHttps + ":" + matchUrl.Groups[1].Value)).GetResponse().Close();
-                            url = Uri.UriSchemeHttps + ":" + matchUrl.Groups[1].Value;
+                            continue;
                         }
-                        catch (Exception)
-                        {
-                            if (matchUrl.Groups[1].Value[0] == '/')
-                            {
-                                url = Uri.UriSchemeHttps + "://" + _uri.Host + matchUrl.Groups[1].Value;
-                            }
-                            else
-                                url = Uri.UriSchemeHttps + "://" + _uri.Host + "/" + matchUrl.Groups[1].Value;
-                        }
+
+                        inAddlist = true;
+                    }
+                    else if (url.Length == 1)
+                    {
+                        url = Uri.UriSchemeHttps + "://" + _uri.Host;
+                        inAddlist = true;
+                    }
+                    else if (url.IndexOf('#', 0, 2) != -1 || url.IndexOf('#', 0, 2) != -1)
+                    {
+                        url = Uri.UriSchemeHttps + "://" + _uri.Host;
+                        inAddlist = true;
                     }
                     else
                     {
-                        url = matchUrl.Groups[1].Value;
+                        if (url[0] == '/')
+                        {
+                            url = Uri.UriSchemeHttps + "://" + _uri.Host + url;
+                            inAddlist = true;
+                        }
+                        else
+                        {
+                            url = Uri.UriSchemeHttps + "://" + _uri.Host + '/' + url;
+                            inAddlist = true;
+                        }
                     }
 
-                    if (!listUrlsHtml.Contains(url))
+                    if (!(url[url.Length - 1] == '/'))
+                        url += '/';
+
+                    if (!listUrlsHtml.Contains(url) && inAddlist)
                     {
                         listUrlsHtml.Add(url);
                     }
+
+                    inAddlist = false;
+                }
+
+                matchCollectionUrls = reg1.Matches(htmlDocument);
+            }
+
+            var listUrls = new List<string>(listUrlsHtml);
+
+            foreach (string url in listUrlsHtml)
+            {
+                if (url != _uri.AbsoluteUri)
+                    await Par(listUrls, url);
+            }
+
+            return listUrls;
+        }
+
+        private async Task Par(List<string> list, string _url)
+        {
+            string htmlDocument;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(_url);
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                using (StreamReader streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    htmlDocument = await streamReader.ReadToEndAsync();
                 }
             }
 
-            return listUrlsHtml;
+            Regex reg = new Regex("<a href=\"(.*?)\"", RegexOptions.Singleline);
+            Regex reg1 = new Regex("<a class=\".*?\" href=\"(.*?)\"", RegexOptions.Singleline);
+            MatchCollection matchCollectionUrls = reg.Matches(htmlDocument);
+
+            var listUrlsHtml = new List<string>(matchCollectionUrls.Count);
+            bool inAddlist = false;
+
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (Match matchUrl in matchCollectionUrls)
+                {
+                    string url = matchUrl.Groups[1].Value;
+
+                    if (url.Contains("mailto:") || url.Contains("skype:"))
+                    {
+                        continue;
+                    }
+                     else if (url.Contains("http://") || url.Contains("https://"))
+                    {
+                        if (!(_uri.Host == new Uri(url).Host))
+                        {
+                            continue;
+                        }
+
+                        inAddlist = true;
+                    }
+                    else if (url.Length == 1)
+                    {
+                        url = Uri.UriSchemeHttps + "://" + _uri.Host;
+                        inAddlist = true;
+                    }
+                    else if (url.IndexOf('#', 0, 2) != -1 || url.IndexOf('#', 0, 2) != -1)
+                    {
+                        url = Uri.UriSchemeHttps + "://" + _uri.Host;
+                        inAddlist = true;
+                    }
+                    else
+                    {
+                        if (url[0] == '/')
+                        {
+                            url = Uri.UriSchemeHttps + "://" + _uri.Host + url;
+                            inAddlist = true;
+                        }
+                        else
+                        {
+                            url = Uri.UriSchemeHttps + "://" + _uri.Host + '/' + url;
+                            inAddlist = true;
+                        }
+                    }
+
+                    if (!(url[url.Length - 1] == '/'))
+                        url += '/';
+
+                    if (!list.Contains(url) && inAddlist)
+                    {
+                        listUrlsHtml.Add(url);
+                        list.Add(url);
+                    }
+
+                    inAddlist = false;
+                }
+
+                matchCollectionUrls = reg1.Matches(htmlDocument);
+            }
+
+            foreach (string url in listUrlsHtml)
+            {
+                await Par(list, url);
+            }
         }
+
+        private List<string> ConcatListUrls(List<string> list1, List<string> list2)
+        {
+            if (list2 != null)
+                foreach (var url in list2)
+                {
+                    if (!list1.Contains(url))
+                    {
+                        list1.Add(url);
+                    }
+                }
+
+            return list1;
+        }
+
     }
 }
