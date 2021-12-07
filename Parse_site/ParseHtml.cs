fd92949abcafbc2adb0afe8d73d8f1e20692c53a
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-07 19:10:04 +0200

    rename ParserHtmlDocument

---
+++ Parse_site/ParseHtml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using ParseDocument;
+
+namespace Parse_site
+{
+    class ParseHtml
+    {
+        private readonly Uri _baseLink;
+        private readonly IParseDocument _parseDocument;
+        private readonly DownloadDocument _downloadDocument = new DownloadDocument();
+
+        public ParseHtml(Uri link, IParseDocument parseDocument)
+        {
+            _baseLink = link;
+            _parseDocument = parseDocument;
+        }
+
+        public List<Uri> StartParse()
+        {
+            var listLinksHtml = new List<Uri>();
+
+            RecursiveParse(listLinksHtml, _baseLink);
+
+            return listLinksHtml;
+        }
+
+        private void RecursiveParse(List<Uri> listOldLinks, Uri startLink)
+        {
+            string document;
+            List<Uri> listLinksFromHtml = null;
+
+            try
+            {
+                document = _downloadDocument.Download(startLink) ?? throw new Exception($"Document html empty for url: {startLink}");
+                listLinksFromHtml = _parseDocument.ParseDocument(document);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            if (listLinksFromHtml == null)
+            {
+                return;
+            }
+
+            var listNewLinks = new List<Uri>();
+
+            foreach (var link in listLinksFromHtml)
+            {
+                if (link != null && !listOldLinks.Contains(link))
+                {
+                    listNewLinks.Add(link);
+                    listOldLinks.Add(link);
+                }
+            }
+
+            foreach (var newLink in listNewLinks)
+            {
+                if (newLink != startLink)
+                {
+                    Console.WriteLine(newLink);
+                    RecursiveParse(listOldLinks, newLink);
+                }
+            }
+        }
+    }
+}
