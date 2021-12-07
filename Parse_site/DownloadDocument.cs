15c48405c37ec34272c401996ffa66b24e4959a2
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-07 19:11:02 +0200

    created download class

---
+++ Parse_site/DownloadDocument.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace Parse_site
+{
+    class DownloadDocument
+    {
+        public string Download(Uri inputLink)
+        {
+            var webClient = new WebClient();
+            webClient.Encoding = Encoding.UTF8;
+
+            string document;
+            
+            try
+            {
+                document = webClient.DownloadString(inputLink);
+            }
+            catch (WebException webEx)
+            {
+                Console.WriteLine(webEx.Message);
+                return null;
+            }
+
+            return document;
+        }
+    }
+}
