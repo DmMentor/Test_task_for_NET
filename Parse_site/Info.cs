3452f502113bf8ca2b7e55b9451ed490cf29ed3c
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-07 20:12:23 +0200

    rename ComparerListUrls

---
+++ Parse_site/Info.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Parse_site
+{
+    class Info : IComparable
+    {
+        public Uri _uri;
+        public int _response;
+
+        public Info(Uri uri, int response)
+        {
+            _uri = uri;
+            _response = response;
+        }
+
+        public int CompareTo(object info)
+        {
+            if (info is Info i)
+            {
+                if (i._response < _response)
+                {
+                    return 1;
+                }
+                else if (i._response > _response)
+                {
+                    return -1;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
