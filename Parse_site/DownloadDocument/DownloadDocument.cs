05fd67249ad775cb89b9acac8013af4f23226573
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-07 16:01:56 +0200

    refactoring class DonwloadDocument

---
+++ Parse_site/DownloadDocument/DownloadDocument.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+
+namespace Parse_site.Download
+{
+    class DownloadDocument
+    {
+        public string Download(Uri inputLink)
+        {
+            var webClient = new WebClient();
+            webClient.Encoding = System.Text.Encoding.UTF8;
+
+            string document;
+
+            try
+            {
+                document = webClient.DownloadString(inputLink);
+            }
+            catch (WebException)
+            {
+                return null;
+            }
+
+            return document;
+        }
+    }
+}
--- Parse_site/DownloadDocument/IDownloadDocument.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Parse_site
-{
-    interface IDownloadDocument<T> where T : class
-    {
-        T DownloadDocument(string inputLink);
-    }
-}
