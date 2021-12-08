22a99554af96cc434b224fd0da222802850ab509
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-08 19:57:08 +0200

    refactoring class Info

---
+++ ParseDocument/Models/Info.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterviewTask.Logic.Models
+{
+    public class Info
+    {
+        public Uri Link { get; set; }
+        public int ResponseTime { get; set; }
+        public bool IsLinkFromHtml { get; set; }
+        public bool IsLinkFromSitemap { get; set; }
+    }
+}
