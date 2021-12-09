23c3779c57d71c6b385b38d4057f83bc95e21a76
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-09 19:15:52 +0200

    wrote tests for DonloadDocument

---
+++ InterviewTask.Logic.Tests/DownloadDocumentTests.cs
@@ -0,0 +1,23 @@
+﻿using InterviewTask.Logic.Parser;
+using NUnit.Framework;
+using System;
+
+namespace InterviewTask.Logic.Tests
+{
+    [TestFixture]
+    class DownloadDocumentTests
+    {
+        [Test]
+        public void Download_UseDontWorkLink_ThrowException()
+        {
+            string expectedString = "Link must absolute";
+            var link = new Uri("ukad-group.com", UriKind.Relative);
+            var downloadDocument = new DownloadDocument();
+
+            TestDelegate actual = () => downloadDocument.Download(link);
+            var exception = Assert.Throws<ArgumentException>(actual);
+
+            Assert.AreEqual(expectedString, exception.Message);
+        }
+    }
+}
