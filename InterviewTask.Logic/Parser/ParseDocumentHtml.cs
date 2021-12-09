fcc63a58c54375a8f144c57e11ca0dcc706a804b
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-09 19:18:36 +0200

    delete method ConvertStringToUri

--- InterviewTask.Logic/Parser/ParseDocumentHtml.cs
+++ InterviewTask.Logic/Parser/ParseDocumentHtml.cs
@@ -1,53 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace InterviewTask.Logic.Parser
 {
     public class ParseDocumentHtml
     {
-        public IEnumerable<Uri> ParseDocument(string document, Uri baseLink)
+        public IEnumerable<string> ParseDocument(string document)
         {
             string conditionalLink = "href=\"";
 
-            IEnumerable<Uri> listLinks = document.Split('\n', '\r')
-                .Where(d => d.Contains("<a") && d.Contains(conditionalLink))
-                .Select(d => ConvertStringToUri(d, conditionalLink, baseLink));
+            IEnumerable<string> listLinks = document?.Split('\n', '\r')
+                .Where(d => d.Contains(conditionalLink))
+                .Select(d => CutLinkString(d, conditionalLink));
 
             return listLinks;
-        }
-
-        private Uri ConvertStringToUri(string inputLink, string conditionalLink, Uri baseLink)
-        {
-            string cutLink = CutLinkString(inputLink, conditionalLink);
-
-            string baseStartLink = baseLink.Scheme + "://" + baseLink.Host;
-
-            Uri link = null;
-
-            if (cutLink.Length <= 1 || cutLink.Contains("/?") || cutLink.Contains("#"))
-            {
-                link = new Uri(baseStartLink);
-            }
-            else if (cutLink.StartsWith("http"))
-            {
-                link = new Uri(cutLink);
-
-                return link.Host == baseLink.Host ? link : null;
-            }
-            else
-            {
-                if (cutLink.Where(s => s == ':').Count() > 0)
-                {
-                    return null;
-                }
-
-                string absolutePath = cutLink.StartsWith('/') ? cutLink : '/' + cutLink;
-
-                link = new Uri(baseStartLink + absolutePath);
-            }
-
-            return link;
         }
 
         private string CutLinkString(string link, string conditionalLink)
