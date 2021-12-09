fc15309964f8955a9a4b44b4528c3dc76707157e
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-09 11:25:15 +0200

    changed the foldername Parse_site

---
+++ InterviewTask.ConsoleApp/ConsoleApp.cs
@@ -0,0 +1,47 @@
+﻿using InterviewTask.Logic.Crawler;
+using InterviewTask.Logic.Services;
+using System;
+
+namespace InterviewTask.ConsoleApp
+{
+    class ConsoleApp
+    {
+        private readonly LinksDisplay _linksDisplay;
+
+        public ConsoleApp(LinksDisplay linksDisplay)
+        {
+            _linksDisplay = linksDisplay;
+        }
+
+        public void Run()
+        {
+            WebsiteCrawlerBuild crawlerBuild = new WebsiteCrawlerBuild();
+
+            try
+            {
+                Console.Write("Input url website: ");
+                string link = Console.ReadLine();
+                //string link = "https://www.ukad-group.com";
+
+                if (!Uri.TryCreate(link, UriKind.Absolute, out Uri linkUri))
+                {
+                    Console.WriteLine("Link is invalid");
+                    return;
+                }
+
+                WebsiteCrawler crawler = crawlerBuild.Build();
+
+                Console.WriteLine($"Starting parsing website....{Environment.NewLine}");
+
+                var listAllLinks = crawler.Start(linkUri);
+
+                _linksDisplay.DisplayResults(listAllLinks);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ InterviewTask.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace InterviewTask.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinksDisplay linksDisplay = new LinksDisplay();
+            ConsoleApp consoleApp = new ConsoleApp(linksDisplay);
+            consoleApp.Run();
+        }
+    }
+}
