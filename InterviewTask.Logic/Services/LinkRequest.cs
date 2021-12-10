f743bc874a34cc9ce5e00c0cf8d9b3a4e7f911aa
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-10 13:35:54 +0200

    create class LinkRequest

---
+++ InterviewTask.Logic/Services/LinkRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+
+namespace InterviewTask.Logic.Services
+{
+    public class LinkRequest
+    {
+        public virtual int LinkResponseTime(Uri link, int timeout = 10000)
+        {
+            if (!link.IsAbsoluteUri)
+            {
+                throw new ArgumentException("Link must absolute");
+            }
+
+            HttpClient client = new HttpClient();
+            client.Timeout = TimeSpan.FromMilliseconds(timeout);
+
+            try
+            {
+                var time = Stopwatch.StartNew();
+
+                using HttpResponseMessage response = client.GetAsync(link).Result;
+
+                time.Stop();
+
+                return time.Elapsed.Milliseconds;
+            }
+            catch (WebException)
+            {
+                return 0;
+            }
+        }
+    }
+}
