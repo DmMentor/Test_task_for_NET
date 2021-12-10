cdbcabf79e34709594a816ae03b480265d947e2f
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-10 13:34:10 +0200

    changes in the design of the test DownloadDocumentTests

--- InterviewTask.Logic.Tests/DownloadDocumentTests.cs
+++ InterviewTask.Logic.Tests/DownloadDocumentTests.cs
@@ -8,15 +8,18 @@ namespace InterviewTask.Logic.Tests
     class DownloadDocumentTests
     {
         [Test]
-        public void Download_UseDontWorkLink_ThrowException()
+        public void Download_UseDontAbsoluteLink_ThrowException()
         {
+            //Arrange
             string expectedString = "Link must absolute";
             var link = new Uri("ukad-group.com", UriKind.Relative);
             var downloadDocument = new DownloadDocument();
-
             TestDelegate actual = () => downloadDocument.Download(link);
+
+            //Act
             var exception = Assert.Throws<ArgumentException>(actual);
 
+            //Assert
             Assert.AreEqual(expectedString, exception.Message);
         }
     }
