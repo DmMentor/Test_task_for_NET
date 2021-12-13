3976778c8737b2e49ae14f79c52f46d28e5201da
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-13 16:08:53 +0200

    [refactoring] class ConsoleApp
    add method SetupCrawler, InputLink and GetResponseLinks

--- InterviewTask.ConsoleApp/ConsoleApp.cs
+++ InterviewTask.ConsoleApp/ConsoleApp.cs
@@ -1,46 +1,93 @@
-﻿using InterviewTask.Logic.Crawler;
+﻿using InterviewTask.Logic.Crawlers;
+using InterviewTask.Logic.Models;
+using InterviewTask.Logic.Parsers;
 using InterviewTask.Logic.Services;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace InterviewTask.ConsoleApp
 {
-    class ConsoleApp
+    internal class ConsoleApp
     {
         private readonly LinksDisplay _linksDisplay;
+        private readonly Converter _converter;
+        private readonly LinkHandling _linkHandling;
 
-        public ConsoleApp(LinksDisplay linksDisplay)
+        public ConsoleApp(LinksDisplay linksDisplay, Converter converter, LinkHandling linkHandling)
         {
             _linksDisplay = linksDisplay;
+            _converter = converter;
+            _linkHandling = linkHandling;
+        }
+
+        private WebsiteCrawler SetupCrawler()
+        {
+            var parseDocumentHtml = new ParseDocumentHtml();
+            var parseDocumentXml = new ParseDocumentSitemap();
+            var downloadDocument = new LinkHandling();
+
+            var parseHtml = new HtmlCrawler(parseDocumentHtml, downloadDocument, _converter);
+            var parseSitemap = new SitemapCrawler(parseDocumentXml, downloadDocument);
+
+            return new WebsiteCrawler(parseHtml, parseSitemap);
         }
 
         public void Run()
         {
-            WebsiteCrawlerBuild crawlerBuild = new WebsiteCrawlerBuild();
-
             try
             {
-                Console.Write("Input url website: ");
-                string link = Console.ReadLine();
-                //string link = "https://www.ukad-group.com";
+                Uri inputLink = InputLink();
 
-                if (!Uri.TryCreate(link, UriKind.Absolute, out Uri linkUri))
+                if (inputLink == null)
                 {
-                    Console.WriteLine("Link is invalid");
                     return;
                 }
 
-                WebsiteCrawler crawler = crawlerBuild.Build();
-
                 Console.WriteLine($"Starting parsing website....{Environment.NewLine}");
 
-                var listAllLinks = crawler.Start(linkUri);
+                WebsiteCrawler crawler = SetupCrawler();
+                var listAllLinks = crawler.Start(inputLink);
 
-                _linksDisplay.DisplayResults(listAllLinks);
+                _linksDisplay.DisplayHtmlLinks(listAllLinks);
+                _linksDisplay.DisplaySitemapLinks(listAllLinks);
 
+                var listAllLinksWithResponse = _converter.ToLinkWithResponse(listAllLinks);
+                GetResponseLinks(listAllLinksWithResponse);
+
+                _linksDisplay.DisplayAllLinks(listAllLinksWithResponse);
+
+                _linksDisplay.DisplayAmountLinks(listAllLinks);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
+            }
+        }
+
+        private Uri InputLink()
+        {
+            Console.Write("Input url website: ");
+            string link = Console.ReadLine();
+
+            if (Uri.TryCreate(link, UriKind.Absolute, out Uri linkUri))
+            {
+                return linkUri;
+            }
+
+            Console.WriteLine("Link is invalid");
+            return null;
+        }
+
+        private void GetResponseLinks(IEnumerable<LinkWithResponse> inputListLinks)
+        {
+            foreach (var link in inputListLinks)
+            {
+                var timer = Stopwatch.StartNew();
+                _linkHandling.GetLinkResponse(link.Url);
+                timer.Stop();
+
+                link.ResponseTime = timer.Elapsed.Milliseconds;
             }
         }
     }
