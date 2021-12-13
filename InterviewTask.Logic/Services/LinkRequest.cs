dacec89cb680b2c31fba04f1fd04c4389583a9af
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-13 17:48:13 +0200

    [create] class LinkRequest

---
+++ InterviewTask.Logic/Services/LinkRequest.cs
@@ -0,0 +1,43 @@
+﻿using InterviewTask.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace InterviewTask.Logic.Services
+{
+    public class LinkRequest
+    {
+        private readonly LinkHandling _linkHandling;
+
+        public LinkRequest(LinkHandling linkHandling)
+        {
+            _linkHandling = linkHandling;
+        }
+
+        public IEnumerable<LinkWithResponse> GetListWithLinksResponseTime(IEnumerable<Link> inputListLinks)
+        {
+            if (inputListLinks == null)
+            {
+                throw new ArgumentNullException("inputList", "List is null");
+            }
+
+            var listLinksWithResponse = new List<LinkWithResponse>();
+
+            foreach (var link in inputListLinks)
+            {
+                LinkWithResponse linkResponse = new LinkWithResponse();
+                linkResponse.Url = link.Url;
+
+                var timer = Stopwatch.StartNew();
+                _linkHandling.GetLinkResponse(link.Url);
+                timer.Stop();
+
+                linkResponse.ResponseTime = timer.Elapsed.Milliseconds;
+
+                listLinksWithResponse.Add(linkResponse);
+            }
+
+            return listLinksWithResponse;
+        }
+    }
+}
