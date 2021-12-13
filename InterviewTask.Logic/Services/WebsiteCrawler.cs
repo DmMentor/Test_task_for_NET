e8eab67f8e8f47e34c6e057e472d6070324a7733
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-13 13:03:16 +0200

    [delete] method TakeTimeResponseLink

--- InterviewTask.Logic/Services/WebsiteCrawler.cs
+++ InterviewTask.Logic/Services/WebsiteCrawler.cs
@@ -3,7 +3,6 @@ using InterviewTask.Logic.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 
 namespace InterviewTask.Logic.Services
 {
@@ -11,19 +10,14 @@ namespace InterviewTask.Logic.Services
     {
         private readonly HtmlCrawler _parserHtml;
         private readonly SitemapCrawler _parserSitemap;
-        private readonly LinkRequest _linkRequest;
-        private readonly int _timeSleep;
 
-        public WebsiteCrawler(HtmlCrawler parseHtml, SitemapCrawler parseSitemap, LinkRequest linkRequest, int timeSleep = 150)
+        public WebsiteCrawler(HtmlCrawler parseHtml, SitemapCrawler parseSitemap)
         {
             _parserHtml = parseHtml;
             _parserSitemap = parseSitemap;
-            _linkRequest = linkRequest;
-
-            _timeSleep = timeSleep;
         }
 
-        public IEnumerable<DataForLink> Start(Uri inputLink)
+        public IEnumerable<Link> Start(Uri inputLink)
         {
             if (!inputLink.IsAbsoluteUri)
             {
@@ -36,52 +30,41 @@ namespace InterviewTask.Logic.Services
 
             var listAllLinks = ConcatLists(listLinksHtml, listLinksSitemap);
 
-            TakeTimeResponseLink(listAllLinks);
-
             return listAllLinks;
         }
 
-        private IEnumerable<DataForLink> ConcatLists(IEnumerable<Uri> listLinksHtml, IEnumerable<Uri> listLinksSitemap)
+        public IEnumerable<Link> ConcatLists(IEnumerable<Uri> listLinksHtml, IEnumerable<Uri> listLinksSitemap)
         {
             var intersectLinks = listLinksHtml.Intersect(listLinksSitemap);
 
-            var listUniqueLinks = intersectLinks.Select(s => new DataForLink()
+            var listUniqueLinks = intersectLinks.Select(s => new Link()
             {
-                Link = s,
+                Url = s,
                 IsLinkFromHtml = true,
                 IsLinkFromSitemap = true
             });
 
             var onlyLinksHtml = listLinksHtml.Except(intersectLinks)
-                .Select(s => new DataForLink()
+                .Select(s => new Link()
                 {
-                    Link = s,
+                    Url = s,
                     IsLinkFromHtml = true,
                     IsLinkFromSitemap = false
                 });
 
             var onlyLinksSitemap = listLinksSitemap.Except(intersectLinks)
-                .Select(s => new DataForLink()
+                .Select(s => new Link()
                 {
-                    Link = s,
+                    Url = s,
                     IsLinkFromHtml = false,
                     IsLinkFromSitemap = true
                 });
 
-            var listAllLinks = new List<DataForLink>(listUniqueLinks);
+            var listAllLinks = new List<Link>(listUniqueLinks);
             listAllLinks.AddRange(onlyLinksHtml);
             listAllLinks.AddRange(onlyLinksSitemap);
 
             return listAllLinks;
-        }
-
-        private void TakeTimeResponseLink(IEnumerable<DataForLink> list)
-        {
-            foreach (var dataForLink in list)
-            {
-                Thread.Sleep(_timeSleep);
-                dataForLink.ResponseTime = _linkRequest.LinkResponseTime(dataForLink.Link);
-            }
         }
     }
 }
