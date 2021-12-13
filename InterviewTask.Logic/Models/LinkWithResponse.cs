45d9e6bd4b4e73e6abac48730d786d34648f3ea5
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-13 13:00:26 +0200

    [create] class LinkWithResponse

---
+++ InterviewTask.Logic/Models/LinkWithResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterviewTask.Logic.Models
+{
+    public class LinkWithResponse
+    {
+        public Uri Url { get; set; }
+        public int ResponseTime { get; set; }
+        public bool IsLinkFromHtml { get; set; }
+        public bool IsLinkFromSitemap { get; set; }
+    }
+}
