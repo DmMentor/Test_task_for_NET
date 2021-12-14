e3b160b977a28ae7c2eaba4ca37044b22f5833ca
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-14 16:54:35 +0200

    [create] class HttpService

---
+++ InterviewTask.Logic/Services/HttpService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace InterviewTask.Logic.Services
+{
+    public class HttpService
+    {
+        private readonly HttpClient _client;
+
+        public HttpService(int timeout = 10000)
+        {
+            _client = new HttpClient
+            {
+                Timeout = TimeSpan.FromMilliseconds(timeout)
+            };
+        }
+
+        public virtual HttpResponseMessage GetResponseMessage(Uri link)
+        {
+            try
+            {
+                return _client.GetAsync(link).Result;
+            }
+            catch (WebException)
+            {
+                return new HttpResponseMessage() { StatusCode = HttpStatusCode.RequestTimeout };
+            }
+        }
+
+        public virtual string GetContent(Uri link)
+        {
+            var response = GetResponseMessage(link);
+            using HttpContent content = response.Content;
+
+            return content.ReadAsStringAsync().Result;
+        }
+    }
+}
