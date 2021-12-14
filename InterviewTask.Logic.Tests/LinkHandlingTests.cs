92148519f0c6a88bb9d6efe629b305e7f5ca4e0a
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-14 16:53:46 +0200

    [add] tests for GetLinkResponse method

--- InterviewTask.Logic.Tests/LinkHandlingTests.cs
+++ InterviewTask.Logic.Tests/LinkHandlingTests.cs
@@ -1,22 +1,34 @@
 ﻿using InterviewTask.Logic.Services;
+using Moq;
 using NUnit.Framework;
 using System;
+using System.Net.Http;
 
 namespace InterviewTask.Logic.Tests
 {
     [TestFixture]
     internal class LinkHandlingTests
     {
+        private Mock<HttpService> _mockClass1;
+        private LinkHandling _linkHandling;
+
+        [SetUp]
+        public void SetUp()
+        {
+            const int timeout = 1;
+            _mockClass1 = new Mock<HttpService>(timeout);
+            _linkHandling = new LinkHandling(_mockClass1.Object);
+        }
+
         [Test]
         public void DownloadDocument_LinkIsNotAbsolute_ThrowException()
         {
             //Arrange
             string expectedString = "Link must be absolute";
             var link = new Uri("ukad-group.com", UriKind.Relative);
-            var linkHandling = new LinkHandling();
 
             //Act
-            var actualException = Assert.Throws<ArgumentException>(() => linkHandling.DownloadDocument(link));
+            var actualException = Assert.Throws<ArgumentException>(() => _linkHandling.DownloadDocument(link));
 
             //Assert
             Assert.AreEqual(expectedString, actualException.Message);
@@ -28,13 +40,42 @@ namespace InterviewTask.Logic.Tests
             //Arrange
             string expectedString = "Link must be absolute";
             var link = new Uri("ukad-group.com", UriKind.Relative);
-            var linkHandling = new LinkHandling();
 
             //Act
-            var actualException = Assert.Throws<ArgumentException>(() => linkHandling.GetLinkResponse(link));
+            var actualException = Assert.Throws<ArgumentException>(() => _linkHandling.GetLinkResponse(link));
 
             //Assert
             Assert.AreEqual(expectedString, actualException.Message);
+        }
+
+        [Test]
+        public void GetLinkResponse_HttpResponseStatusCodeOk_ReturnResponseTime()
+        {
+            //Arrange
+            var baseLink = new Uri("https://test1.com");
+            var returnsResponseMessage = new HttpResponseMessage() { StatusCode = System.Net.HttpStatusCode.OK };
+            _mockClass1.Setup(m => m.GetResponseMessage(It.IsAny<Uri>())).Returns(returnsResponseMessage);
+
+            //Act
+            var actual = _linkHandling.GetLinkResponse(baseLink);
+
+            //Assert
+            Assert.IsTrue(actual >= 0);
+        }
+
+        [Test]
+        public void GetLinkResponse_HttpResponseStatusCodeRequestTimeout_ReturnNegativeNumber()
+        {
+            //Arrange
+            var baseLink = new Uri("https://test1.com");
+            var returnsResponseMessage = new HttpResponseMessage() { StatusCode = System.Net.HttpStatusCode.RequestTimeout };
+            _mockClass1.Setup(m => m.GetResponseMessage(It.IsAny<Uri>())).Returns(returnsResponseMessage);
+
+            //Act
+            var actual = _linkHandling.GetLinkResponse(baseLink);
+
+            //Assert
+            Assert.IsTrue(actual == -1);
         }
     }
 }
