9fb879fcb492d33308524ae80cea3c2dba86e935
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-16 13:20:40 +0200

    [create] ServiceExtensions class

---
+++ InterviewTask.Logic/Extensions/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using InterviewTask.Logic.Crawlers;
+using InterviewTask.Logic.Parsers;
+using InterviewTask.Logic.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InterviewTask.Logic.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddInterviewTaskLogicServices(this IServiceCollection services)
+        {
+            services.AddScoped<WebsiteCrawler>();
+            services.AddScoped<HtmlCrawler>();
+            services.AddScoped<SitemapCrawler>();
+            services.AddScoped<ParseDocumentHtml>();
+            services.AddScoped<ParseDocumentSitemap>();
+            services.AddScoped<Converter>();
+            services.AddScoped<HttpService>();
+            services.AddScoped<LinkHandling>();
+            services.AddScoped<LinkRequest>();
+
+            return services;
+        }
+    }
+}
