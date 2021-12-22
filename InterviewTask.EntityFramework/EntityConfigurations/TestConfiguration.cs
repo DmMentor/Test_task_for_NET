003e650d9eeb3d3aeff7ad2916e7604fd5518460
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-22 15:51:56 +0200

    [create] configuration for entity Test

---
+++ InterviewTask.EntityFramework/EntityConfigurations/TestConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using InterviewTask.EntityFramework.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InterviewTask.EntityFramework.EntityConfigurations
+{
+    internal class TestConfiguration : IEntityTypeConfiguration<Test>
+    {
+        public void Configure(EntityTypeBuilder<Test> builder)
+        {
+            builder.Property(p => p.ParsingDate)
+                   .HasPrecision(0)
+                   .ValueGeneratedOnAdd()
+                   .HasDefaultValueSql("GETUTCDATE()");
+        }
+    }
+}
