81d5a96c6ecbe3d6767e823906ed636224fdc0f0
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-22 15:50:45 +0200

    [create] entity CrawlingResult

---
+++ InterviewTask.EntityFramework/Entities/CrawlingResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InterviewTask.EntityFramework.Entities
+{
+    public class CrawlingResult
+    {
+        public int Id { get; set; }
+        public Uri Url { get; set; }
+        public int ResponseTime { get; set; }
+        public bool IsLinkFromHtml { get; set; }
+        public bool IsLinkFromSitemap { get; set; }
+
+        public int TestId { get; set; }
+        public Test Test { get; set; }
+    }
+}
