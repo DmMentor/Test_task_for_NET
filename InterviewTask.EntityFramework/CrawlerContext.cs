c7d6e47a295fa9e28bc5e6a1d0d5ddc14b809aac
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-22 15:51:13 +0200

    [create] database context CrawlerContext

---
+++ InterviewTask.EntityFramework/CrawlerContext.cs
@@ -0,0 +1,22 @@
+﻿using InterviewTask.EntityFramework.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Reflection;
+
+namespace InterviewTask.EntityFramework
+{
+    public class CrawlerContext : DbContext, IEfRepositoryDbContext
+    {
+        public DbSet<CrawlingResult> Links { get; set; }
+        public DbSet<Test> Tests { get; set; }
+
+        public CrawlerContext(DbContextOptions<CrawlerContext> dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
