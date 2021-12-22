a42df89f9af2390be42b07e4eca072d7bc9c27a1
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-22 15:51:42 +0200

    [create] configuration for entity CrawlingResult

---
+++ InterviewTask.EntityFramework/EntityConfigurations/CrawlingResultConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using InterviewTask.EntityFramework.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InterviewTask.EntityFramework.EntityConfigurations
+{
+    internal class CrawlingResultConfiguration : IEntityTypeConfiguration<CrawlingResult>
+    {
+        public void Configure(EntityTypeBuilder<CrawlingResult> builder)
+        {
+            builder.Property(p => p.IsLinkFromHtml);
+            builder.Property(p => p.IsLinkFromSitemap);
+            builder.Property(p => p.Url).HasColumnName("Links");
+        }
+    }
+}
