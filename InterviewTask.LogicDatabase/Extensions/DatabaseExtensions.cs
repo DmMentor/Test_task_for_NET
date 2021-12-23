f2b918e9b93d7528f0450d8dd04bc0a29192d30b
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-23 15:40:34 +0200

    [create] class DatabaseExtensions

---
+++ InterviewTask.LogicDatabase/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,15 @@
+﻿using InterviewTask.LogicDatabase.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InterviewTask.LogicDatabase.Extensions
+{
+    public static class DatabaseExtensions
+    {
+        public static IServiceCollection AddLogicDatabaseServices(this IServiceCollection services)
+        {
+            services.AddScoped<DatabaseOperation>();
+
+            return services;
+        }
+    }
+}
