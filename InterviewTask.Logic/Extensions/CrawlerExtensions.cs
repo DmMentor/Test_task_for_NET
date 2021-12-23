c0235eb295f0d0ad5df289a87c0b1089d19334c7
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-23 13:34:07 +0200

    [rename] class ServiceExtensions

---
+++ InterviewTask.Logic/Extensions/CrawlerExtensions.cs
@@ -0,0 +1,25 @@
+﻿using InterviewTask.LogicCrawler.Crawlers;
+using InterviewTask.LogicCrawler.Parsers;
+using InterviewTask.LogicCrawler.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InterviewTask.LogicCrawler.Extensions
+{
+    public static class CrawlerExtensions
+    {
+        public static IServiceCollection AddLogicCrawlerServices(this IServiceCollection services)
+        {
+            services.AddScoped<WebsiteCrawler>()
+                    .AddScoped<HtmlCrawler>()
+                    .AddScoped<SitemapCrawler>()
+                    .AddScoped<ParseDocumentHtml>()
+                    .AddScoped<ParseDocumentSitemap>()
+                    .AddScoped<Converter>()
+                    .AddScoped<HttpService>()
+                    .AddScoped<LinkHandling>()
+                    .AddScoped<LinkRequest>();
+
+            return services;
+        }
+    }
+}
