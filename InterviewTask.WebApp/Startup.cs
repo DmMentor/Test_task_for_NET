11bb4ba07615548369dad299b323612615cb9529
Dmitry Minaev <dm.minaev03@gmail.com>
2021-12-29 12:58:17 +0200

    [create] web app project

---
+++ InterviewTask.WebApp/Startup.cs
@@ -0,0 +1,58 @@
+using InterviewTask.CrawlerServices.Extensions;
+using InterviewTask.DatabaseServices.Extensions;
+using InterviewTask.EntityFramework;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace InterviewTask.WebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddEfRepository<CrawlerContext>(o =>
+             {
+                 o.UseSqlServer(Configuration.GetConnectionString("Connection"));
+             });
+            services.AddCrawlerServices();
+            services.AddDatabaseServices();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Crawler}/{action=Input}/{id?}");
+            });
+        }
+    }
+}
