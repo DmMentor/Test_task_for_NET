3200eb4179439e469fa48993570d25e0cfa686e9
Dmitry Minaev <dm.minaev03@gmail.com>
2022-01-04 16:25:54 +0200

    [create] service LinkValidator

---
+++ InterviewTask.WebApp/Services/LinkValidator.cs
@@ -0,0 +1,35 @@
+﻿using InterviewTask.WebApp.Models;
+using System;
+using System.Net;
+
+namespace InterviewTask.WebApp.Services
+{
+    public class LinkValidator
+    {
+        public TestResult CheckLink(Uri link)
+        {
+            var testResult = new TestResult();
+            testResult.Message = "Parsing completed successfully";
+
+            if (link == null)
+            {
+                testResult.Message = "Input value equal null";
+                testResult.IsValid = false;
+            }
+            else
+            {
+                try
+                {
+                    WebRequest.Create(link).GetResponse().Close();
+                }
+                catch
+                {
+                    testResult.Message = "Link dont work";
+                    testResult.IsValid = false;
+                }
+            }
+
+            return testResult;
+        }
+    }
+}
