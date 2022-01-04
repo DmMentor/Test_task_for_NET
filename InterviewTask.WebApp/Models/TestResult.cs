f7e559841cafbace55555f2248ec57ccede7be8a
Dmitry Minaev <dm.minaev03@gmail.com>
2022-01-04 16:26:18 +0200

    [create] model TestResult

---
+++ InterviewTask.WebApp/Models/TestResult.cs
@@ -0,0 +1,8 @@
+﻿namespace InterviewTask.WebApp.Models
+{
+    public class TestResult
+    {
+        public bool IsValid { get; set; } = true;
+        public string Message { get; set; }
+    }
+}
