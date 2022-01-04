3cd4c81091c2d2d8f1e1980e73e512a9e7bbee43
Dmitry Minaev <dm.minaev03@gmail.com>
2022-01-04 16:27:17 +0200

    [create] model ErrorViewModel

---
+++ InterviewTask.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InterviewTask.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
