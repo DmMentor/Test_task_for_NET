858eb8c548e6bb7ea8ad47058327f882eb7eb492
Dmitry Minaev <dm.minaev03@gmail.com>
2022-01-04 16:26:09 +0200

    [create] model Result

---
+++ InterviewTask.WebApp/Models/Result.cs
@@ -0,0 +1,21 @@
+﻿using InterviewTask.EntityFramework.Entities;
+using System.Collections.Generic;
+
+namespace InterviewTask.WebApp.Models
+{
+    public class Result
+    {
+        public IEnumerable<Test> List { get; set; }
+        public int Page { get; set; } = 1;
+        public int TotalCount { get; set; }
+        public int PageSize { get; set; }
+        public bool IsLastPage
+        {
+            get => PageSize * Page >= TotalCount;
+        }
+        public bool IsFirstPage
+        {
+            get => Page <= 1;
+        }
+    }
+}
