704d702d3f828a7135ba998dd3c7950aee32638a
Dmitry Minaev <dm.minaev03@gmail.com>
2022-01-28 08:28:33 +0200

    [create] web api project

---
+++ InterviewTask.WepApi/Startup.cs
@@ -0,0 +1,66 @@
+using InterviewTask.CrawlerLogic.Extensions;
+using InterviewTask.EntityFramework;
+using InterviewTask.Logic.Extensions;
+using InterviewTask.WepApi.Middlewares;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace InterviewTask.WepApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "InterviewTask.WepApi", Version = "v1" });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            services.AddEfRepository<CrawlerContext>(o =>
+            {
+                o.UseSqlServer(Configuration.GetConnectionString("Connection"));
+            });
+            services.AddCrawlerServices();
+            services.AddLogicServices();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "InterviewTask.WepApi v1"));
+            }
+
+            app.UseMiddleware<ExceptionHandlerMiddleware>();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
